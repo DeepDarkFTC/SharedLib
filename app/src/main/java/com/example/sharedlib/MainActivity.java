4a4b671e4bb09c0d7c6345be7675c4858f00d0e4
weiguangm <weiguangm@student.unimelb.edu.au>
2019-09-27 17:31:07 +1000

    Modified by Weiguang Ma

--- app/src/main/java/com/example/sharedlib/MainActivity.java
+++ app/src/main/java/com/example/sharedlib/MainActivity.java
@@ -1,29 +1,77 @@
 package com.example.sharedlib;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button login;
+    private TextView userName;
+    private TextView password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = (Button) findViewById(R.id.login_button);
+        userName = findViewById(R.id.name);
+        password = findViewById(R.id.password);
 
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                Log.v("test","press button");
-                System.out.println("teest");
+                // send message to firebase
+                String message = "";     // message from firebase, may phase to several attributes
+
+                if (checkFormat(userName.getText().toString(), password.getText().toString())) {
+
+                    Intent intent = new Intent(MainActivity.this, HomePageActivity.class);
+                    startActivity(intent);
+                }
             }
         });
+    }
+
+    public Boolean checkFormat(String userName, String password) {
+        String email = "\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
+        if (userName.equals("")) {
+            new AlertDialog.Builder(this)
+                    .setTitle("warning")
+                    .setMessage("Username cannot be empty")
+                    .setPositiveButton("ok", null)
+                    .show();
+            return false;
+        }
+        if (!userName.matches(email)) {
+            new AlertDialog.Builder(this)
+                    .setTitle("warning")
+                    .setMessage("Username should be an email")
+                    .setPositiveButton("ok", null)
+                    .show();
+            return false;
+        }
+        if (password.equals("")) {
+            new AlertDialog.Builder(this)
+                    .setTitle("warning")
+                    .setMessage("Password cannot be empty")
+                    .setPositiveButton("ok", null)
+                    .show();
+            return false;
+        }
+        if (password.length() < 6) {
+            new AlertDialog.Builder(this)
+                    .setTitle("warning")
+                    .setMessage("The length of Password should larger than 6")
+                    .setPositiveButton("ok", null)
+                    .show();
+            return false;
+        }
+        return true;
     }
 }
