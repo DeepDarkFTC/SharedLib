63c0756072487da6b292a4e4eccc84a501df19c3
weiguangm <weiguangm@student.unimelb.edu.au>
2019-09-27 15:22:17 +1000

    Modified by Weiguang Ma

--- app/src/main/java/com/example/sharedlib/LoginActivity.java
+++ app/src/main/java/com/example/sharedlib/LoginActivity.java
@@ -2,13 +2,32 @@ package com.example.sharedlib;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.content.Intent;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login = (Button)findViewById(R.id.login_button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
