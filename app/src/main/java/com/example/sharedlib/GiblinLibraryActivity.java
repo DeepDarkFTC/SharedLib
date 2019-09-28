9efde176e0b387f416546b005622904e6f73029a
weiguangm <weiguangm@student.unimelb.edu.au>
2019-09-28 12:13:25 +1000

    Modified by Weiguang Ma

---
+++ app/src/main/java/com/example/sharedlib/GiblinLibraryActivity.java
@@ -0,0 +1,14 @@
+package com.example.sharedlib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GiblinLibraryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.giblin);
+    }
+}
